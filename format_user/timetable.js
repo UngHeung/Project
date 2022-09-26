2916003303202e5f2595f2774366d92dd9675ed0
ungHeung <t01047377477@gmail.com>
2022-09-26 23:20:29 +0900

    timetable.js 수정

--- format_user/timetable.js
+++ format_user/timetable.js
@@ -17,6 +17,11 @@ function addBarLine() {
 }
 
 addBarLine();
+/**
+ * 48시간을 30분 간격으로 표시
+ * 1시간 간격으로 긴 침
+ * 6시간 간격으로 굵은 침, 시간 표시
+ */
 
 // 일 시간 select옵션 생성
 const dateSelect = document.getElementById("time-date");
@@ -40,6 +45,10 @@ function addOption() {
 }
 
 addOption();
+/**
+ * select 태그 옵션수가 많으므로 코드 양을 줄이기 위해
+ * for 반복문을 사용하여 옵션을 추가
+ */
 
 // 시간, 분 값 저장
 let dateSelectVal = "none";
@@ -47,25 +56,28 @@ let hourSelectVal = 1;
 let minuteSelectVal = 0;
 dateSelect.addEventListener("change", () => {
     dateSelectVal = dateSelect.value;
-    hourSelectVal = parseInt(hourSelect.value);
-    minuteSelectVal = parseInt(minuteSelect.value);
 });
 hourSelect.addEventListener("change", () => {
-    dateSelectVal = dateSelect.value;
     hourSelectVal = parseInt(hourSelect.value);
-    minuteSelectVal = parseInt(minuteSelect.value);
 });
 minuteSelect.addEventListener("change", () => {
-    dateSelectVal = dateSelect.value;
-    hourSelectVal = parseInt(hourSelect.value);
     minuteSelectVal = parseInt(minuteSelect.value);
 });
+/**
+ * select 박스의 선택 값이 바뀔 때마다
+ * 바뀐 값을 선언된 변수에 저장
+ * 초기값은 시간은 1시간, 분은 0분
+ */
 
 // 버튼
-const addColor = document.querySelectorAll(".radio-color"); // 박스 색상
+const addColor = document.querySelectorAll(".radio-color");
 const addBoxCont = document.querySelector(".box-cont");
 const addBtn = document.querySelector(".add-box"); // 박스 추가
 const delBtn = document.querySelector(".del-box"); // 박스 삭제
+/**
+ * addColor => 선택된 색상
+ * addBoxCont => 타임테이블에 입력할 간단한 문자열
+ */
 
 let width;
 let height;
@@ -89,6 +101,9 @@ for (let i = 0; i < addColor.length; i++) {
         }
     });
 }
+/**
+ * 색상 라디오버튼이 클릭될 때마다 addColor 변수가 갖는 색상 값이 변경됨
+ */
 
 /* 박스 추가 */
 function addBox() {
@@ -136,7 +151,14 @@ function addBox() {
 
     countBox++;
 }
-console.log(dateSelectVal);
+/**
+ * 너비는 80px 고정, 높이는 시간당 30px으로 다음과 같이 구함
+ * hourSelectVal(선택된 시간) * 30 + minuteSelectVal(선택된 분)
+ * 추가 버튼을 클릭할 때마다 지정된 너비와 높이, 색상 가진 div 태그에
+ * addboxCont 변수가 가진 값을 입력 후
+ * 선택된 일자의 자리에 추가
+ */
+
 addBtn.addEventListener("click", () => {
     if (dateSelectVal == "none") {
         alert("몇일인지 선택해주세요");
@@ -145,3 +167,7 @@ addBtn.addEventListener("click", () => {
     addBox();
     addBoxCont.value = "";
 });
+/**
+ * 일자가 선택되지 않았을 경우
+ * 알림창 띄움
+ */
